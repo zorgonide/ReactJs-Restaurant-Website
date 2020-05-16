ec8a549100a9fb03f80af00ea39a11e4160fd6e8
zorgonide <ammar1998@gmail.com>
2020-05-16 23:55:41 +0530

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
+import Menu from './components/MenuComponent';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 
@@ -12,6 +13,7 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu />
       </div>
     );
   }
