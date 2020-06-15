fad2df3fabf98458ac7d161d9fd3a8467d0896a9
zorgonide <ammar1998@gmail.com>
2020-06-15 20:32:00 +0530

    Final Changes

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Breadcrumb, BreadcrumbItem,
     Button, Row, Col, Label } from 'reactstrap';
-import { Control, Form, Errors, actions } from 'react-redux-form';
+import { Control, Form, Errors } from 'react-redux-form';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -34,10 +34,7 @@ const mapDispatchToProps = dispatch => ({
 
 class Main extends Component {
 
-  constructor(props) {
-    super(props);
-  }
-
+ 
   componentDidMount(){
     this.props.fetchDishes();
     this.props.fetchComments();
