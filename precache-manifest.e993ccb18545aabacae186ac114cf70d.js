29b05b7fbb84b7eaef5368511fe6402f078d6929
zorgonide <ammar1998@gmail.com>
2020-06-15 21:06:47 +0530

    Updates

---
+++ precache-manifest.e993ccb18545aabacae186ac114cf70d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cb3190befdfeb4017835851af66c80e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54efc3bb46c89cdb1b9c",
+    "url": "/static/css/2.7f0a60d8.chunk.css"
+  },
+  {
+    "revision": "fe353a9a1e33a87815e0",
+    "url": "/static/css/main.bc2a2813.chunk.css"
+  },
+  {
+    "revision": "54efc3bb46c89cdb1b9c",
+    "url": "/static/js/2.5891d46f.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.5891d46f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe353a9a1e33a87815e0",
+    "url": "/static/js/main.6d152376.chunk.js"
+  },
+  {
+    "revision": "79298229a104881fd483",
+    "url": "/static/js/runtime-main.ad013ee6.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
